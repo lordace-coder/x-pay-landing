40083d79806944a76793959a86f37e7ab727c8a5
Diego <iwujigoodluck5@gmail.com>
2025-06-07 09:01:28 +0000

    done with importation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { useEffect } from "react";
+
+// Styles 
+import "../assets/vendors/bootstrap/bootstrap.min.css";
+import "../assets/vendors/bootstrap-icons/font/bootstrap-icons.min.css";
+import "../assets/vendors/glightbox/glightbox.min.css";
+import "../assets/vendors/swiper/swiper-bundle.min.css";
+import "../assets/vendors/aos/aos.css";
+// End styles 
+
+// Javascript and libraries
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { gsap } from "gsap";
 import imagesLoaded from "imagesloaded";
@@ -12,6 +22,8 @@ import "swiper/css/pagination";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import PureCounter from "@srexi/purecounterjs";
+// end of javascript and libraries 
+
 
 export default function HomePage() {
   useEffect(() => {
