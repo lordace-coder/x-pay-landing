db229d1d4814fea8040f8f35c33154fe7ae42ef5
lordace-coder <lordyacey@gmail.com>
2025-10-03 23:53:09 +0200

    ll

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,2 +1,2 @@
-export const BASEURL = "http://127.0.0.1:8000";
-// export const BASEURL = "https://xpay-api-murex.vercel.app";
+// export const BASEURL = "http://127.0.0.1:8000";
+export const BASEURL = "https://xpay-api-murex.vercel.app";
